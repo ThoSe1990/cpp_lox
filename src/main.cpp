69f893ad501e0a867316ba90cea4a9be4c827ea2
Thomas <thomas.sedlmair@yunextraffic.com>
2023-08-05 17:09:40 +0200

    parser done

--- src/main.cpp
+++ src/main.cpp
@@ -33,44 +33,37 @@ std::string readFile(const std::string& filename)
 }
 
 
+#include "expr.hpp"
+#include "parser.hpp"
+
 void run(const std::string& src) 
 {
   cwt::scanner scanner(src);
   std::vector<cwt::token> tokens = scanner.scan_tokens();
 
-  for (const auto& t : tokens){
-    std::cout << t.to_string() << '\n';
+  cwt::parser<std::string> parser(tokens);
+  cwt::expression<std::string>* expr = parser.parse(); 
+
+  if (expr) {
+    cwt::printer().print(*expr);
+    delete expr;
   }
 }
 
-#include "expr.hpp"
 
 int main(int argc, char** argv)
 {
-  using expr = cwt::expression<std::string>;
+  if (argc == 1) {
 
-  expr* l1 = new cwt::expr_literal<std::string>("123");
-  expr* l2 = new cwt::expr_literal<std::string>("45.67");
+  } else if (argc == 2) {
+    const std::string path{argv[1]};
+    std::cout << "reading: " << path << '\n';
+    run(readFile(path));
+  } else {
+    std::cerr << "invalid argc given\n";
+    return -1;  
+  }
 
-  expr* unary = new cwt::expr_unary<std::string>(
-      cwt::token(cwt::token_type::MINUS, "-", 1),
-      l1
-    );
-
-  expr* gr = new cwt::expr_grouping<std::string>(l2);
-
-  cwt::expr_binary<std::string> exp(
-    unary,
-    cwt::token(cwt::token_type::STAR, "*", 1),
-    gr
-  );
-
-  cwt::printer().print(exp);
-
-  // delete l1; 
-  // delete l2;
-  // delete unary;
-  // delete gr; 
-
+  std::cout << "program done!\n";
   return 0;
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,19 +1,204 @@
 #pragma once 
 
 namespace cwt
-{
+{ 
+  template<typename T> 
   class parser
   {
+    using value_t = T;
+    using expr_t = expression<value_t>;
+    
     public: 
       parser(const std::vector<token>& tokens) : m_tokens(tokens) {}
 
-      expression* parse() 
+      expr_t* parse() 
       {
-
+        try
+        {
+          return expression();
+        }
+        catch(const std::exception& e)
+        {
+          std::cout << e.what() << '\n';
+          return nullptr;
+        }
       }
 
     private:
+      expr_t* expression() 
+      {
+        return equality();
+      }
+      expr_t* equality() 
+      {
+        expr_t* expr = comparison();
+        while (match(token_type::BANG_EQUAL, token_type::EQUAL_EQUAL))
+        {
+          token op = previous();
+          expr_t* right = comparison();
+          expr = new expr_binary<value_t>(expr, op, right);
+        }
+        return expr; 
+      }
 
+      bool match(token_type t)
+      {
+        if (check(t)) {
+          advance();
+          return true;
+        }
+        return false;
+      }
+
+      template<typename... T>
+      bool match(token_type head, T... ts)
+      {
+        return match(head) ? true : match(ts...);
+      }
+
+      bool check(token_type t)
+      {
+        if (is_at_end())
+        {
+          return false;
+        }
+        else 
+        {
+          return peek().m_type == t;
+        }
+      }
+
+      token advance()
+      {
+        if (!is_at_end()) ++m_current;
+        return previous();
+      }
+
+      bool is_at_end() 
+      {
+        return peek().m_type == token_type::END_OF_FILE;
+      }
+
+      token peek()
+      {
+        return m_tokens[m_current];
+      }
+
+      token previous()
+      {
+        return m_tokens[m_current-1];
+      }
+
+      expr_t* comparison()
+      {
+        expr_t* expr = term();
+        while(match(token_type::GREATER, token_type::GREATER_EQUAL, token_type::LESS, token_type::LESS_EQUAL))
+        {
+          token op = previous();
+          expr_t* right = term();
+          expr = new cwt::expr_binary<value_t>(expr, op, right);
+        }
+        return expr;
+      }
+
+      expr_t* term()
+      {
+        expr_t* expr = factor();
+        while(match(token_type::MINUS, token_type::PLUS))
+        {
+          token op = previous();
+          expr_t* right = factor();
+          expr = new expr_binary<value_t>(expr, op, right);
+        }
+        return expr; 
+      }
+
+      expr_t* factor()
+      {
+        expr_t* expr = unary();
+        while (match(token_type::SLASH, token_type::STAR))
+        {
+          token op = previous();
+          expr_t* right = unary();
+          expr = new expr_binary<value_t>(expr, op, right);
+        }
+        return expr; 
+      }
+
+      expr_t* unary()
+      {
+        if (match(token_type::BANG, token_type::MINUS))
+        {
+          token op = previous();
+          expr_t* right = unary();
+          return new expr_unary<value_t>(op, right);
+        }
+        return primary();
+      }
+
+      expr_t* primary()
+      {
+        if (match(token_type::FALSE)) return new expr_literal<value_t>("false");
+        if (match(token_type::TRUE)) return new expr_literal<value_t>("true");
+        if (match(token_type::NIL)) return new expr_literal<value_t>("nil");
+        if (match(token_type::NUMBER, token_type::STRING))
+        {
+          return new expr_literal<value_t>(previous().m_literal);
+        }
+        if (match(token_type::LEFT_PAREN))
+        {
+          expr_t* expr = expression();
+          consume(token_type::RIGHT_PAREN, "Expect: \')\' after expression.");
+          return new expr_grouping<value_t>(expr);
+        }
+        throw std::runtime_error(error(peek(), "Expected expression."));
+      }
+
+      token consume(token_type type, const std::string& msg) 
+      {
+        if(check(type)) return advance();
+        
+        throw std::runtime_error(error(peek(), msg));
+      }
+
+      std::string error(token t, const std::string& msg) 
+      {
+        if (t.m_type == token_type::END_OF_FILE) 
+        {
+          report(t.m_line, " at end ", msg);
+        }
+        else 
+        {
+          std::string s{"at \' "};
+          s.append(t.m_lexeme);
+          s.append("\'");
+          report(t.m_line, s, msg);
+        }
+        
+        return msg; 
+      }
+
+      void synchronize() 
+      {
+        advance();
+        while (!is_at_end())
+        {
+          if (previous().m_type == token_type::SEMICOLON) return; 
+          switch (peek().m_type)
+          {
+          case token_type::CLASS :
+          case token_type::FOR :
+          case token_type::FUN :
+          case token_type::IF :
+          case token_type::PRINT :
+          case token_type::RETURN :
+          case token_type::VAR :
+          case token_type::WHILE :
+            return;
+          }
+          advance();
+        }
+      }
 
     private:
       std::size_t m_current{0};
